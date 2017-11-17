6cff24894ba5cf5d7f493523b220d559a5505c1b
TJ Loughry <tjloughry@gmail.com>
2017-11-17 11:41:25 -0600

    preping react files

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div class="frame">
+          <div class="name">
+            <div class="firstName">
+              <div class="tt">
+                <div class="line tt-top"></div>
+                <div class="line tt-down"></div>
+              </div>
+              <div class="jj">
+                <div class="line jj-bottom"></div>
+                <div class="line jj-down"></div>
+              </div>
+            </div>
+            <div class="lastName">
+              <div class="ll">
+                <div class="line ll-bottom"></div>
+                <div class="line ll-down"></div>
+              </div>
+
+
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
